71e69eb5b7e5fb82155824385769effb7822fe67
reyhansaja <raihan.ts16b@gmail.com>
2025-02-18 09:42:04 +0700

    Mahasiswa 23 dan Mahasiswa Main

---
+++ pertemuan_2/Mahasiswa23.java
@@ -0,0 +1,48 @@
+public class Mahasiswa23 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi(){
+        System.out.println("Nama: "+nama);
+        System.out.println("NIM: "+nim);
+        System.out.println("IPK: "+ipk);
+        System.out.println("Kelas: "+kelas);
+    }
+    void ubahKelas(String kelasBaru){
+        kelas = kelasBaru;
+    }
+    void updateIpk(double ipkBaru){
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        }
+        else{
+            System.out.println("IPK Tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
+    String nilaiKinerja(){
+        if(ipk>=3.5){
+            return "Kinerja Bagus";
+        }
+        else if(ipk >= 3.0){
+            return "Kinerja Baik";
+        }
+        else if(ipk >= 2.0){
+            return "Kinerja Cukup";
+        }
+        else{
+            return "Kinerja Kurang";
+        }
+    }
+    // public Mahasiswa23(){
+
+    // }
+    // public Mahasiswa23(String nm, String nim, double ipk, String kls){
+    //     nama = nm;
+    //     this.nim = nim;
+    //     this.ipk = ipk;
+    //     kelas = kls;
+    // }
+}
+
