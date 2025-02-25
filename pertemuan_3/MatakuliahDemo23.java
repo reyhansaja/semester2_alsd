5e1ed8eb97a61d658e63c00510eb92ec38a84fb7
reyhansaja <raihan.ts16b@gmail.com>
2025-02-25 10:45:41 +0700

    ubah matakuliah

--- pertemuan_3/MatakuliahDemo23.java
+++ pertemuan_3/MatakuliahDemo23.java
@@ -3,24 +3,11 @@ public class MatakuliahDemo23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Matakuliah23[] arrayMatakuliah23 = new Matakuliah23[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
 
         for (int i = 0; i < 3; i++) {
-            System.out.println("Masukkan data matakuliah ke- "+(i+1));
-            System.out.print("Kode: ");
-            kode = sc.nextLine();
-            System.out.print("Nama: ");
-            nama = sc.nextLine();
-            System.out.print("SKS: ");
-            dummy = sc.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah jam: ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("----------------------");
-
-            arrayMatakuliah23[i] = new Matakuliah23(kode,nama,sks,jumlahJam);
+            System.out.println("Masukkan data matakuliah ke- " + (i+1));
+            arrayMatakuliah23[i] = new Matakuliah23("","",0,0);
+            arrayMatakuliah23[i].tambahData(sc);
         }
         for (int i = 0; i < 3; i++) {
             System.out.println("Data Mata Kuliah ke-"+ (i+1));
