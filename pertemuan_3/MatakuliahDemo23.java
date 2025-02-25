45953cce72bbc111d6ee3ee74640c745dcbaded7
reyhansaja <raihan.ts16b@gmail.com>
2025-02-25 11:00:13 +0700

    tambah jumlah data

--- pertemuan_3/MatakuliahDemo23.java
+++ pertemuan_3/MatakuliahDemo23.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 public class MatakuliahDemo23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matakuliah23[] arrayMatakuliah23 = new Matakuliah23[3];
+        int jumlahData;
+        System.out.print("Jumlah data: ");
+        jumlahData = sc.nextInt();
+        Matakuliah23[] arrayMatakuliah23 = new Matakuliah23[jumlahData];
 
-        for (int i = 0; i < 3; i++) {
+
+        for (int i = 0; i < jumlahData; i++) {
             System.out.println("Masukkan data matakuliah ke- " + (i+1));
             arrayMatakuliah23[i] = new Matakuliah23("","",0,0);
             arrayMatakuliah23[i].tambahData(sc);
         }
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlahData; i++) {
             System.out.println("Data mata kuliah ke-"+ (i+1));
             arrayMatakuliah23[i].cetakInfo();
         }
