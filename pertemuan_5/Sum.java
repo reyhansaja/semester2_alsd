ee9b91e3afe8f58fa01cf40f55eae27889f1a7af
reyhansaja <raihan.ts16b@gmail.com>
2025-03-11 10:58:31 +0700

    masukkan sum.java

---
+++ pertemuan_5/Sum.java
@@ -0,0 +1,26 @@
+public class Sum {
+    double keuntungan[];
+
+    Sum(int el){
+        keuntungan =  new double[el];
+    }
+
+    double TotalBF(){
+        double total = 0;
+        for (int i=0; i<keuntungan.length; i++) {
+            total = total + keuntungan[i];
+        }
+        return total;
+    }
+
+    double TotalDC(double arr[], int l, int r){
+        if (l == r) {
+            return arr[1];
+        }
+
+        int mid = (l+r)/2;
+        double lsum = TotalDC(arr, l, mid);
+        double rsum = TotalDC(arr, mid+1, r);
+        return lsum+rsum;
+    }
+}
