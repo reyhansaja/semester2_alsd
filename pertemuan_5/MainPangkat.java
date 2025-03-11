023482d1a7200db53f93b7a698be622c052e3ac2
reyhansaja <raihan.ts16b@gmail.com>
2025-03-11 10:28:50 +0700

    tambah isian

--- pertemuan_5/MainPangkat.java
+++ pertemuan_5/MainPangkat.java
@@ -8,7 +8,20 @@ public class MainPangkat {
 
         Pangkat[] png = new Pangkat[elemen];
         for (int i=0;i<elemen;i++) {
-            System.out.println("Masukkan basis elemen ke-"+(i+1)": " );
+            System.out.print("Masukkan basis elemen ke-"+(i+1)+": " );
+            int basis = input.nextInt();
+            System.out.print("Masukkan pangkat elemen ke-"+(i+1)+": ");
+            int pangkat = input.nextInt();
+            png[i] = new Pangkat(basis, pangkat);
+        }
+        
+        System.out.println("Hasil pangkat bruteforce: ");
+        for(Pangkat p : png){
+            System.out.println(p.nilai+"^"+p.pangkat+": "+p.PangkatBF(p.nilai, p.pangkat));
+        }
+        System.out.println("Hasil pangkat devide and conquer: ");
+        for(Pangkat p : png){
+            System.out.println(p.nilai+"^"+p.pangkat+": "+p.PangkatDC(p.nilai, p.pangkat));
         }
     }
 }
