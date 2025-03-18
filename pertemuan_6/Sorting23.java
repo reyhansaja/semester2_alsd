8923de4bac6657565f9a654a755092b2644e78c5
reyhansaja <raihan.ts16b@gmail.com>
2025-03-18 08:57:38 +0700

    perbaiki error

--- pertemuan_6/Sorting23.java
+++ pertemuan_6/Sorting23.java
@@ -14,7 +14,7 @@ public class Sorting23 {
     void bubleSort(){
         int temp = 0;
         for(int i=0; i<jumData-1; i++){
-            for(int j=1; j<jumData-i; i++){
+            for(int j=1; j<jumData-i; j++){
                 if(data[j-1]>data[j]){
                     temp = data[j];
                     data[j] = data[j-1];
@@ -26,7 +26,7 @@ public class Sorting23 {
 
     void tampil(){
         for (int i=0; i<jumData; i++){
-            System.out.println(data[i]+ " ");
+            System.out.print(data[i]+ " ");
         }
         System.out.println();
     }
