81832900dc5f1a4111c517d11349398892d21467
reyhansaja <raihan.ts16b@gmail.com>
2025-03-18 11:57:46 +0700

    edan

--- pertemuan_6/MahasiswaBerprestaasi23.java
+++ pertemuan_6/MahasiswaBerprestaasi23.java
@@ -47,4 +47,16 @@ public class MahasiswaBerprestaasi23 {
             listMhs[i]=tmp;
         }
     }
+
+    void insertionSort(){
+        for(int i=0; i<listMhs.length;i++){
+            Mahasiswa23 temp = listMhs[i];
+            int j=i;
+            while (j>0 && listMhs[j-1].ipk>temp.ipk) {
+                listMhs[j]=listMhs[j-1];
+                j--;
+            }
+            listMhs[j]=temp;
+        }
+    }
 }
