f68200f85980f9d996f3a471517e3a4653969c38
reyhansaja <raihan.ts16b@gmail.com>
2025-03-18 11:39:39 +0700

    gatau

--- pertemuan_6/MahasiswaBerprestaasi23.java
+++ pertemuan_6/MahasiswaBerprestaasi23.java
@@ -33,4 +33,18 @@ public class MahasiswaBerprestaasi23 {
             }  
         }
     }
+
+    void selectionSort(){
+        for(int i=0; i<listMhs.length; i++){
+            int idxMin=i;
+            for(int j=i+1; j<listMhs.length; j++){
+                if(listMhs[j].ipk<listMhs[idxMin].ipk){
+                    idxMin=j;
+                }
+            }
+            Mahasiswa23 tmp = listMhs[idxMin];
+            listMhs[idxMin]=listMhs[i];
+            listMhs[i]=tmp;
+        }
+    }
 }
