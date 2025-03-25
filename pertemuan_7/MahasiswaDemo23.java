c3aa0dfd66c5af6387a881d677a48c9c732f8e6e
reyhansaja <raihan.ts16b@gmail.com>
2025-03-25 11:13:01 +0700

    tambah demo

--- pertemuan_7/MahasiswaDemo23.java
+++ pertemuan_7/MahasiswaDemo23.java
@@ -35,5 +35,19 @@ public class MahasiswaDemo23 {
         int pss=(int)posisi;
         list.tampilPosisi(cari, pss);
         list.tampilDataSearch(cari, pss);
+
+        System.out.println("-----------------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("-----------------------------------------------");
+        System.out.println("masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari2 = sc.nextDouble();
+        System.out.println("----------------------------");
+        System.out.println("Menggunakan binary search");
+        System.out.println("----------------------------");
+        double posisi2 = list.findBinarySearch(cari2, 0, jumMhs-1);
+        int pss2 = (int)posisi2;
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari, pss2);
     }
 }
