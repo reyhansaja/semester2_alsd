f4499d705145db320be2decc4ec13b5d09850fc2
reyhansaja <raihan.ts16b@gmail.com>
2025-05-20 09:34:07 +0700

    fix

--- pertemuan_12/doublelinkedlist/DoubleLinkedList.java
+++ pertemuan_12/doublelinkedlist/DoubleLinkedList.java
@@ -26,7 +26,7 @@ public class DoubleLinkedList {
             while (current.next != null) {
                 current = current.next;
             }
-            Node newNode = new Node (current, item, null);
+            Node newNode = new Node (current, item, head);
             current.next = newNode;
             size++;
         }
