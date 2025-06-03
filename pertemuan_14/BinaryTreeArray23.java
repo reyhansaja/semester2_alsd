f9c078c27a363fe9ec4fc7d8f7cb43ec2464266e
reyhansaja <raihan.ts16b@gmail.com>
2025-06-03 11:22:49 +0700

    tambah array

---
+++ pertemuan_14/BinaryTreeArray23.java
@@ -0,0 +1,23 @@
+public class BinaryTreeArray23 {
+    Mahasiswa23[] dataMahasiswa;
+    int idxLast;
+
+    public BinaryTreeArray23(){
+        this.dataMahasiswa = new Mahasiswa23[10];
+    }
+
+    void populatedData(Mahasiswa23 dataMhs[], int idxLast){
+        this.dataMahasiswa = dataMhs;
+        this.idxLast = idxLast;
+    }
+
+    void traverseInOrder (int idxStart){
+        if(idxStart <= idxLast){
+            if (dataMahasiswa[idxStart] != null) {
+                traverseInOrder(2*idxStart+1);
+                dataMahasiswa[idxStart].tampilInformasi();
+                traverseInOrder(2*idxStart+2);
+            }
+        }
+    }
+}
---
+++ pertemuan_14/BinaryTreeArrayMain23.java
@@ -0,0 +1,3 @@
+public class BinaryTreeArrayMain23 {
+    
+}
